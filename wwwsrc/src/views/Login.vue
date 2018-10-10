ec32d2fa766f213341420560aca5d389e82134c9
michael <michael.green94@hotmail.com>
2018-10-09 23:47:01 -0600

    login structuring

--- wwwsrc/src/views/Login.vue
+++ wwwsrc/src/views/Login.vue
@@ -5,24 +5,39 @@
             <v-toolbar-title>Keepr</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn @click="loginForm = !loginForm" flat>Login</v-btn>
-                <v-btn @click="registerForm = !registerForm" flat>Register</v-btn>
+                <v-btn @click="showForms = !showForms" flat>Login/Register</v-btn>
             </v-toolbar-items>
         </v-toolbar>
-        <v-layout>
-            <v-form ref="form" v-if="loginForm">
-                <v-text-field v-model="creds.email" @submit.prevent="loginUser" :rules="emailRules" label="E-mail" required></v-text-field>
-                <v-text-field v-model="creds.password" :rules="passwordRules" :counter="20" label="Password" required></v-text-field>
-                <v-btn :disabled="!valid" @click="loginUser">submit</v-btn>
-                <v-btn @click="clear">clear</v-btn>
-            </v-form>
-            <v-form ref="form" v-if="registerForm">
-                <v-text-field v-model="newUser.username" :rules="usernameRules" label="Username" required></v-text-field>
-                <v-text-field v-model="newUser.email" :rules="emailRules" label="E-mail" required></v-text-field>
-                <v-text-field v-model="newUser.password" :rules="passwordRules" :counter="20" label="Password" required></v-text-field>
-                <v-btn :disabled="!valid" @click="register">Create Account</v-btn>
-                <v-btn @click="clear">clear</v-btn>
-            </v-form>
+        <v-layout row justify-center>
+            <v-dialog v-model="showForms" scrollable width="45rem" transition="scale-transition">
+                <v-card>
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12 sm6 md12>
+                                <v-form ref="form" v-if="loginForm">
+                                    <v-btn @click="showForms = false" flat>X</v-btn>
+                                    <v-text-field v-model="creds.email" @submit.prevent="loginUser" :rules="emailRules" label="E-mail" required></v-text-field>
+                                    <v-text-field v-model="creds.password" :rules="passwordRules" :counter="20" label="Password" required></v-text-field>
+                                    <v-btn :disabled="!valid" @click="loginUser">Login</v-btn>
+                                    <v-btn @click="clear">clear</v-btn>
+                                </v-form>
+                                <v-form ref="form" v-else>
+                                    <v-btn @click="showForms = false" flat>X</v-btn>
+                                    <v-text-field v-model="newUser.username" :rules="usernameRules" label="Username" required></v-text-field>
+                                    <v-text-field v-model="newUser.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                                    <v-text-field v-model="newUser.password" :rules="passwordRules" :counter="20" label="Password" required></v-text-field>
+                                    <v-btn :disabled="!valid" @click="register">Register</v-btn>
+                                    <v-btn @click="clear">clear</v-btn>
+                                </v-form>
+                                <div @click="loginForm = !loginForm">
+                                    <p v-if="loginForm">No account Click to Register</p>
+                                    <p v-else>Already have an account click to Login</p>
+                                </div>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+            </v-dialog>
         </v-layout>
     </div>
 </template>
@@ -37,7 +52,7 @@ export default {
   data() {
     return {
       loginForm: false,
-      registerForm: false,
+      showForms: false,
       creds: {
         email: "",
         password: ""
