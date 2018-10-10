9fdfc576b5cba43fb5c2be8106aa2932cc296bd0
michael <michael.green94@hotmail.com>
2018-10-09 21:57:18 -0600

    updated packages

--- wwwsrc/src/main.js
+++ wwwsrc/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 
